2fddc8d55f45b576e2d5dc627f2cf394bdc901fc
MaherMuhtadi <m.muhtadi2002@gmail.com>
2023-01-23 08:28:27 +0000

    Issue Fix: Toggling theme also affects images now

--- script.js
+++ script.js
@@ -2,10 +2,18 @@ function toggle_theme() {
     /**
      * toggles between light and dark theme of the site
      */
+    let images = document.getElementsByTagName("img");
+    
     if (document.body.classList.contains("light")) {
         document.body.classList.remove("light");
+        for (var i = 0; i < images.length; i++) {
+            images[i].src = images[i].src.replace("/light/", "/dark/");
+        }
     }
     else {
         document.body.classList.add("light");
+        for (var i = 0; i < images.length; i++) {
+            images[i].src = images[i].src.replace("/dark/", "/light/");
+        }
     }
 }
